c8fff188c655f4bc0bdf187283121a14921bee7e
Yuri <Yuri.Chaves39@hotmail.com>
2022-02-28 11:59:54 -0300

    Commit 02.28.2022 11:59

---
+++ AulaMVC/AulaMVC/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AulaMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage ="Campo Obrigatório")]
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        [Display(Name = "Id da Categoria")]
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
